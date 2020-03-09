7bf36dc2902532aa2aed7014c2e89d91257e658e
maycol mancilla <mancillamaycol@gmail.com>
2020-03-09 10:58:18 -0400

    examen1

---
+++ EXAMEN/EXAMEN1/EXAMEN1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMEN1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Memor de {0}, {1} y {2} es {3}\n",
+                    
+                    2, 3, 4, Menor( 1 2, 3, 4));
+                Console.WriteLine("Menor de {0}, {1} y {2} es {3}\n",
+                   3.1, 4.6, 2.3, Menor(1.3, 4.6, 2.3));
+                Console.WriteLine("Menor de {0}, {1} y {2} es {3}\n",
+                   "juan ", "pedro", "maria", Menor("juan ", "pedro ", " maria"));
+                Console.ReadKey();
+            }
+        }
+        private static T Menor<T>(T x, T y, T z)
+           where T : IComparable<T>
+        {
+            T max = x; //
+            if (y.CompareTo(max) > 0)
+                max = y; 
+            
+            if (z.CompareTo(max) > 0)
+                max = z; 
+            return max; 
+        }
+    }
+}
---
+++ EXAMEN/EXAMEN2/EXAMEN2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMEN2
+{
+    public class Operaciones // Esta es la clase publicadora
+    {
+        public delegate void Delegado(int n); // lafirma del deleado debe ocnincidir con la del Manejador de eventod
+        public event Delegado CuandoRecibaMultiplosCinco; //evento aqui se suscriben lo manejadores de eventos 
+        public int Sumar(int x, int y)
+        {
+            int iSuma = x + y;
+            if ((iSuma % 5 == 0) && (CuandoRecibaMultiplosCinco != null)) // cuando ocurre el evento de sermultiplo de 5 tenemos que llamar a evento para dispare enlos suscriptores
+            { CuandoRecibaMultiplosCinco(iSuma); }
+            return iSuma;
+        }
+    }
+    class ProgramaEvento
+    {
+        static void Main()
+        {
+            Operaciones o = new Operaciones();
+            o.CuandoRecibaMultiplosCinco += RecibeMultiploCinco;// nos estamos suscribiendo a eventoy mandandoalmanejador de eventos
+            o.CuandoRecibaMultiplosCinco += RecibeMultiploCinco;// nos estamos suscribiendo a eventoy mandandoalmanejador de eventos
+
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            int res = o.Sumar(a, b);
+            Console.WriteLine("La suma es = {0}", res);
+            Console.ReadKey();
+        }
+        static void RecibeMultiploCinco(int n) // Manejador de Evento
+        {
+        }
+    }
+}
+
+   
